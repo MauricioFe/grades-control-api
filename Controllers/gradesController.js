e5fdb74d5d8f6406910a9f8cb9c00a7b8be8efd5
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-08 12:42:12 -0300

    refatoração de código

---
+++ Controllers/gradesController.js
@@ -0,0 +1,45 @@
+import { promises } from "fs";
+const { readFile, writeFile } = promises;
+
+async function inserirGrades(grade){
+    if (!grade.student || !grade.subject || !grade.type || !grade.value) {
+        throw new Error("Preencha todos os campos como requerido");
+    }
+
+    const data = JSON.parse(await readFile(global.fileName));
+    grade = {
+        id: data.nextId++,
+        student: grade.student,
+        subject: grade.subject,
+        type: grade.type,
+        value: grade.value,
+        timestamp: new Date
+    };
+    data.grades.push(grade);
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    return grade;
+}
+
+
+async function editarGrades(grade){
+    const data = JSON.parse(await readFile(global.fileName));
+
+    const index = data.grades.findIndex(g => g.id === grade.id);
+
+    if (index === -1) {
+        throw new Error("Grade não encontrado");
+    }
+    if (!grade.student || !grade.subject || !grade.type || !grade.value) {
+        throw new Error("Preencha todos os campos como requerido");
+    }
+
+    data.grades[index].student = grade.student;
+    data.grades[index].subject = grade.subject;
+    data.grades[index].type = grade.type;
+    data.grades[index].value = grade.value;
+    data.grades[index].timestamp = new Date;
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    return grade;
+}
+
+export  default inserirGrades
--- Routes/grades.js
+++ Routes/grades.js
@@ -1,29 +1,13 @@
 import express from "express";
 import { promises as fs } from "fs";
+import inserirGrades from "../Controllers/gradesController.js";
 const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
     try {
-        let grade = req.body;
-
-        if (!grade.student || !grade.subject || !grade.type || !grade.value) {
-            throw new Error("Preencha todos os campos como requerido");
-        }
-
-        const data = JSON.parse(await readFile(global.fileName));
-        grade = {
-            id: data.nextId++,
-            student: grade.student,
-            subject: grade.subject,
-            type: grade.type,
-            value: grade.value,
-            timestamp: new Date
-        };
-        data.grades.push(grade);
-        await writeFile(global.fileName, JSON.stringify(data, null, 2));
-        res.send(grade);
+        res.send(await inserirGrades(req.body));
     } catch (err) {
         console.log(err)
     }
@@ -42,23 +26,7 @@ router.get("/", async (req, res) => {
 router.put("/", async (req, res) => {
     try {
         let grade = req.body;
-        const data = JSON.parse(await readFile(global.fileName));
-
-        const index = data.grades.findIndex(g => g.id === grade.id);
-
-        if (index === -1) {
-            throw new Error("Grade não encontrado");
-        }
-        if (!grade.student || !grade.subject || !grade.type || !grade.value) {
-            throw new Error("Preencha todos os campos como requerido");
-        }
-
-        data.grades[index].student = grade.student;
-        data.grades[index].subject = grade.subject;
-        data.grades[index].type = grade.type;
-        data.grades[index].value = grade.value;
-        data.grades[index].timestamp = new Date;
-        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+       
         res.send(grade);
     } catch (error) {
         console.log(error)
